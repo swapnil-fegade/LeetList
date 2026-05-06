f32872386cdadb7a2b3e5c08f073b0c6bba704c9
swapnil-fegade <143727207+swapnil-fegade@users.noreply.github.com>
2026-05-06 20:41:21 +0530

    Time: 23 ms (28.07%) | Memory: 80.9 MB (75.79%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num :nums){
+            set.add(num);
+        }
+
+        return (set.size() < nums.length) ? true : false;
+    }
+}
