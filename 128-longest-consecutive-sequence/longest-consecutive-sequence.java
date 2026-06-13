e66b840cdf17429819c2a9cb7b880193afcb4dda
swapnil-fegade <143727207+swapnil-fegade@users.noreply.github.com>
2026-06-13 19:46:33 +0530

    Time: 30 ms (78.35%) | Memory: 95.8 MB (28.29%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int num : nums){    // nums to set for faster lookup
+            set.add(num);
+        }
+        int maxLen = 0;
+        for(int num: set){
+            if(!set.contains(num-1)){
+                int len = 1;
+
+                while(set.contains(num+len)){
+                    len++;
+                }
+
+                maxLen = Math.max(maxLen, len);
+            }
+        }
+        return maxLen;
+    }
+}
