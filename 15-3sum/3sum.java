2f1b97430d829af610a05f649310cdf66e06976b
swapnil-fegade <143727207+swapnil-fegade@users.noreply.github.com>
2026-06-14 22:48:33 +0530

    Time: 31 ms (83.15%) | Memory: 59 MB (84.68%) - LeetSync

---
+++ 15-3sum/3sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i=0;i<n-2;i++){
+
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
+
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum < 0){
+                    j++;
+                } else if( sum>0){
+                    k--;
+                } else{
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while(j<k && nums[j] == nums[j+1]) j++;
+                    j++;
+                }
+            }
+        }
+        return res;
+    }
+}
